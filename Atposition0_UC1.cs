65c25dbad8e2570b867699d34b2aba7b1f049abd
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-15 16:47:50 +0530

    Update Atposition0_UC1.cs

--- Atposition0_UC1.cs
+++ Atposition0_UC1.cs
@@ -27,53 +27,7 @@ namespace SnakeandLadder
                 }
                 Console.WriteLine("You are now at position {0}", position);
 
-                // check for snakes and ladders
-                switch (position)
-                {
-                    case 3:
-                        Console.WriteLine("You found a ladder at position 3! You climb to position 21.");
-                        position = 21;
-                        break;
-                    case 8:
-                        Console.WriteLine("You found a ladder at position 8! You climb to position 30.");
-                        position = 30;
-                        break;
-                    case 17:
-                        Console.WriteLine("You found a snake at position 17! You slide down to position 13.");
-                        position = 13;
-                        break;
-                    case 28:
-                        Console.WriteLine("You found a snake at position 28! You slide down to position 19.");
-                        position = 19;
-                        break;
-                    case 52:
-                        Console.WriteLine("You found a ladder at position 52! You climb to position 72.");
-                        position = 72;
-                        break;
-                    case 57:
-                        Console.WriteLine("You found a snake at position 57! You slide down to position 40.");
-                        position = 40;
-                        break;
-                    case 62:
-                        Console.WriteLine("You found a snake at position 62! You slide down to position 22.");
-                        position = 22;
-                        break;
-                    case 75:
-                        Console.WriteLine("You found a ladder at position 75! You climb to position 86.");
-                        position = 86;
-                        break;
-                    case 88:
-                        Console.WriteLine("You found a snake at position 88! You slide down to position 52.");
-                        position = 52;
-                        break;
-                }
-
-                Console.WriteLine("Press Enter key to roll the dice...");
-                Console.ReadLine();
             }
-
-            Console.WriteLine("Congratulations! You reached position 100 and won the game!");
-            Console.ReadLine();
         }
 }
 }
